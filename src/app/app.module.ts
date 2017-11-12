444f1a8ef20593422ec32939c3d467ee17c92020
pomtcom <pomtcom@hotmail.com>
2017-11-12 22:57:30 +0700

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TestComponent2Component } from './test-component-2/test-component-2.component';
+
+const appRoutes: Routes = [
+  { path: 'test1', component: TestComponentComponent },
+  { path: 'test2', component: TestComponent2Component },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent
+    TestComponentComponent,
+    TestComponent2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/test-component-2/test-component-2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component-2',
+  templateUrl: './test-component-2.component.html',
+  styleUrls: ['./test-component-2.component.css']
+})
+export class TestComponent2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
